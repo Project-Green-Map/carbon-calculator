6d02520eedaaed1e93b3ecbbe24c872c532012bd
Miro Macko <miro.macko@nozdormu.net>
2022-02-15 19:06:48 +0000

    restructure

--- calculator.py
+++ calculator.py
@@ -1,6 +1,6 @@
 """ Supposed to communicate with the server """
 
 
-def calculation(distance):
+def calculation(distance, transport_type):
     return distance * 123
 
--- main.py
+++ main.py
@@ -1,26 +1,5 @@
-from distance_getter import calculation
+from calculator import calculation
 from enum import Enum
-
-class Transport_method(Enum):
-    WALKING = 1
-    CYCLING = 2
-    CAR = 3
-    PUBLIC_TRANSPORT = 4
-
-class Data:
-    def __init__(self, JSON_file):
-        self.distance = 2
-        self.type = Transport_method.CAR
-        self.model = "DIESEL_TESLA"
-
-    def consumption_perModel(self):  ### gets the model and returns the CO2 emissions per km
-        if(self.model=="DIESEL_TESLA"):
-            return 10.5
-        return 7.5
-    
-    def compute_emmisions(self):
-        return self.distance * self.consumption_perModel()
-
     
 
 def main(request):
@@ -33,22 +12,15 @@ def main(request):
         `make_response <http://flask.pocoo.org/docs/1.0/api/#flask.Flask.make_response>`.
     """
     request_json = request.get_json()
-    if request.args and 'message' in request.args:
-        return request.args.get('message') + "don't want this"
-    elif request_json:
+    if request_json:
         # here we want to write stuff, request_json is just the dictionary representing the json
+
         if "distance" not in request_json:
             return "sorry, can't do a calculation"
 
-        d = request_json["distance"]
+        #d = request_json["distance"]
         
-        return str(calculation(d))
+        return 2 #str(calculation(d))
     else:
-        return f'We have a problem'
+        return f'We have a problem - no json received'
 
-    JSON_file = "example.JSON"
-    new_data = Data(JSON_file)
-    print(new_data.compute_emmisions())
-    print()
-
-main()
