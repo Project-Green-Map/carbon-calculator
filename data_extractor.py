bac044400de7ea55406b1be399d9c92278fe1f5e
Horea <hms70@cam.ac.uk>
2022-02-15 19:47:58 +0000

    implement basic calculation for cars and public_transport

--- data_extractor.py
+++ data_extractor.py
@@ -1,24 +1,41 @@
 from openpyxl import load_workbook
 import pandas
+all_data = {}
 
 def extract_data(): 
     workbook = load_workbook(filename="CO2_data.xlsx")
     spreadsheet = workbook.active
-    print(spreadsheet["D3"].value)
 
-    dataframe = pandas.read_excel(io="C:/Users/Horea/Project_Green_Maps_CO2_CalculatorProject_Green_Maps_CO2_Calculator/carbon-calculator/CO2_data.xlsx", sheet_name="Sheet1")
-    print(dataframe)
+    ### Extracting car data 
+    car_dataframe = pandas.read_excel(io="C:/Users/Horea/Project_Green_Maps_CO2_CalculatorProject_Green_Maps_CO2_Calculator/carbon-calculator/CO2_data.xlsx", sheet_name="Cars")
 
-
-    
-    dict={}
+    car = {}
     for i in range(1,9,2):
         fuels = {}
-        fuels["Diesel"] = dataframe.loc[i,"Diesel"]
-        fuels["Petrol"] = dataframe.loc[i,"Petrol"]
-        fuels["Hybrid"] = dataframe.loc[i,"Hybrid"]
-        dict[dataframe.iloc[i,1]] = fuels
-    
-    print(dict)
+        fuels["Diesel"] = car_dataframe.loc[i,"Diesel"]
+        fuels["Petrol"] = car_dataframe.loc[i,"Petrol"]
+        fuels["Hybrid"] = car_dataframe.loc[i,"Hybrid"]
+        car[car_dataframe.iloc[i,1]] = fuels
+    all_data["car"] = car
+
+    ### Extracting public transport data 
+    public_transport_dataframe =  pandas.read_excel(io="C:/Users/Horea/Project_Green_Maps_CO2_CalculatorProject_Green_Maps_CO2_Calculator/carbon-calculator/CO2_data.xlsx", sheet_name="Public_transport")
+
+   
+    public_transport = {}
+    for i in range (0,public_transport_dataframe.shape[0]):
+        public_transport[public_transport_dataframe.iloc[i,0]] = public_transport_dataframe.iloc[i,1]
+    all_data["public_transport"] = public_transport
+
+    all_data["walinkg"] = 0
+    all_data["cycling"] = 0.005
+    print(all_data)
+
+def get_car_value(transport_type, car_type, fuel_type):
+    return all_data["car"][car_type][fuel_type]
+
+def get_public_transport_value(transport_type):
+    return all_data["public_transport"][transport_type]
 
 extract_data()
+print(get_car_value("car","Average car","Diesel"))
