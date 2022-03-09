cf4e8e5cde0029616a802d25a8b2411f2f341a64
Miro Macko <miro.macko@nozdormu.net>
2022-03-09 09:11:32 +0000

    adding processing_data folder

---
+++ processing_data/merge_dicts.py
@@ -0,0 +1,31 @@
+'''
+Given brand_dicts (also called Make_dicts at some places)
+It will produce a merged dict and printout any conflicts that
+should be resolved manually.
+'''
+
+
+import json
+
+dict_paths = [
+    "Make_dict_1.json",
+    "Make_dict_2.json"
+]
+dictionaries=[]
+
+for path in dict_paths:
+    with open(path) as brand_dict_file:
+        dictionaries.append(json.load(brand_dict_file))
+
+
+result_dict = dictionaries[0]
+for i in range(1, len(dictionaries)):
+    for key, val in dictionaries[i].items():
+        if key in result_dict:
+            if val != result_dict[key]:
+                print(f"found a conflict: key: {key}, val: {val},  val present: {result_dict[key]}")
+        else:
+            result_dict[key] = val
+
+with open("Make_dict.json", "w") as outfile:
+    json.dump(result_dict, outfile, indent=4, sort_keys=True)
---
+++ processing_data/update_dicts.py
@@ -0,0 +1,35 @@
+'''
+Given a master brand_dict (also called Make_dict at some places)
+It will update any other brand_dictionaries with the updated master values.
+'''
+
+import json
+
+dict_paths = [
+    "Make_dict_1.json",
+    "Make_dict_2.json"
+]
+
+dict_master_path = "Make_dict.json" #current version
+
+dictionaries=[]
+
+for path in dict_paths:
+    with open(path) as brand_dict_file:
+        dictionaries.append(json.load(brand_dict_file))
+
+dict_master = {}
+with open(dict_master_path) as brand_dict_file:
+    dict_master = json.load(brand_dict_file)
+
+
+for i in range(len(dictionaries)):
+    for key, val in dict_master.items():
+        if key in dictionaries[i]:
+            if val != dictionaries[i][key]:
+                print(f"found a conflict: key: {key}, val: {val},  val2: {dictionaries[i][key]}")
+                dictionaries[i][key] = val #update
+        
+for i in range(len(dictionaries)):
+    with open(dict_paths[i], "w") as outfile:
+       json.dump(dictionaries[i], outfile, indent=4, sort_keys=True)
